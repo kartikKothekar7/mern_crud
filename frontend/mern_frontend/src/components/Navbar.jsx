d3a848c32a6b4232a403cadc78134ee768188d21
ETRNLxFLAME <kartikkothekar7@gmail.com>
2026-01-22 17:12:51 +0530

    navbar

--- frontend/mern_frontend/src/components/Navbar.jsx
+++ frontend/mern_frontend/src/components/Navbar.jsx
@@ -1,21 +1,9 @@
-// import React from 'react'
 
-// const Navbar = () => {
-//   return (
-//     <div>
-//         <div className='w-screen bg-gray-500 flex flex-row h-2rem item-center'>
-//             <a href="http://localhost:5173/login">login</a>
-//             <a href="http://localhost:5173/signup">signup</a>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Navbar
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const token=localStorage.getItem("token");
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -25,6 +13,8 @@ const Navbar = () => {
   return (
     <nav className="w-screen bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-semibold">Notes App</h1>
+      <a href="http://localhost:5173/login">login</a>
+      <a href="http://localhost:5173/signup">signup</a>
 
       <button
         onClick={handleLogout}
@@ -32,8 +22,6 @@ const Navbar = () => {
       >
         Logout
       </button>
-      <a href="http://localhost:5173/login">login</a>
-      <a href="http://localhost:5173/signup">signup</a>
     </nav>
   );
 };
